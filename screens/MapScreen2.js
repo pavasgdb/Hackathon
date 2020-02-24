fbf88474930decc59d65d3497f57b240818dbd07
Bhavesh Gupta <aforaspire501@gmail.com>
2020-02-24 22:14:26 +0530

    Location

---
+++ screens/MapScreen2.js
@@ -0,0 +1,129 @@
+import React, { Component } from 'react';
+
+import {
+    View,
+    StyleSheet,
+    Dimensions,
+} from 'react-native';
+
+import MapView from 'react-native-maps';
+import Images from "../constants/Images";
+
+
+const { width, height } = Dimensions.get('window')
+
+const SCREEN_HEIGHT = height
+const SCREEN_WIDTH = width
+const ASPECT_RATIO = width / height
+const LATITUDE_DELTA = 0.006
+const LONGITUDE_DELTA = 0.003
+
+class MapScreen extends Component {
+    constructor() {
+        super()
+        this.state = {
+            initialPosition: {
+                latitude: 0,
+                longitude: 0,
+                latitudeDelta: 0,
+                longitudeDelta: 0,
+            },
+        }
+    }
+    componentDidMount() {
+        this.interval = setInterval(() => this.tick(), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+    componentDidMount() {
+        this.interval = setInterval(() => {
+            navigator.geolocation.getCurrentPosition((position) => {
+                var lat = parseFloat(position.coords.latitude)
+                var long = parseFloat(position.coords.longitude)
+
+                var initialRegion = {
+                    latitude: lat,
+                    longitude: long,
+                    latitudeDelta: LATITUDE_DELTA,
+                    longitudeDelta: LONGITUDE_DELTA,
+                }
+
+                this.setState({ initialPosition: initialRegion })
+            },
+                (error) => alert(JSON.stringify(error)),
+                { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 });
+        }, 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+    renderScreen = () => {
+        console.log(this.state.initialPosition)
+        return (
+            <View style={styles.container}>
+                <MapView
+                    coordinate={this.state.initialPosition}
+                    region={this.state.initialPosition}
+                    style={styles.mapStyle}
+                //showsMyLocationButton={true}
+                >
+                    <MapView.Marker
+                        coordinate={this.state.initialPosition}
+                        // {latitude:global.variable[0].coords.latitude,longitude:global.variable[0].coords.longitude
+                        title={'abcd'}
+                        description={'1234'}
+                        image={Images.Marker} />
+                </MapView>
+                <View style={{
+                    position: 'absolute',
+                    right: 0,
+                    top: 0,
+                    backgroundColor: 'grey',
+                    alignItems: 'center',
+                    justifyContent: 'space-around',
+                    padding: 20,
+                    width: 100,
+                    height: 100,
+                    borderBottomLeftRadius: 5
+                }}>
+                    <View>
+                        <Image source={{ uri: 'http://openweathermap.org/img/w/' + '04n' + '.png' }} style={{ width: 50, height: 50, resizeMode: 'stretch' }} />
+                    </View>
+                    {/* <Text>
+                        Temp -->
+                       </Text> */}
+                    <Text style={{ fontSize: 15, }}>
+                        {this.state.temp}
+                    </Text>
+                </View>
+            </View>
+        );
+    }
+    render() {
+        return (
+            this.renderScreen()
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    mapStyle: {
+        width: '100%',
+        height: '100%',
+        flex: 1,
+    },
+});
+
+export default MapScreen;
